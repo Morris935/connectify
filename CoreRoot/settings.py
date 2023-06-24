569ac3520bcbc50809a91bf18736be4b23d54413
Morris935 <ngowamorris18@students.ku.ac.ke>
2023-06-24 20:51:22 +0300

    Adding user model

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'core.user',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Tell django to use this User model for the authentication user model.
+AUTH_USER_MODEL = 'core_user.user'
